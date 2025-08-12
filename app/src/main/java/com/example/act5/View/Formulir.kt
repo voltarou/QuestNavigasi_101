40c6f15b6cc06e5e2613329a9b202cb7c838fd3c
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-12 14:54:37 +0700

    5

---
+++ app/src/main/java/com/example/act5/View/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.act4.View
+
+import androidx.compose.runtime.Composable
+
+@OptIn(...markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = Listof("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+) {
+}
